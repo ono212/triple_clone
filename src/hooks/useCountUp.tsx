e07a3809ece01a342f8d5e1b85b1656be2e92c50
jungmin <qkrwjdals302@naver.com>
2022-07-09 19:17:23 +0900

    refactor: 함수의 위치 변경

--- src/hooks/useCountUp.tsx
+++ src/hooks/useCountUp.tsx
@@ -1,1 +1,25 @@
-export default function useCountUp() {}
+import { useEffect, useState } from 'react'
+
+function easeOutExpo(t: number): number {
+  return t === 1 ? 1 : 1 - Math.pow(2, -10 * t)
+}
+
+export default function useCountUp(end: number, start = 0, duration = 2000) {
+  const [count, setCount] = useState(start)
+  const frameRate = 1000 / 60 // 숫자를 갱신하는 주기 (초) === 몇 초에 한 번씩 숫자를 갱신할건지
+  const totalFrame = Math.round(duration / frameRate) // 갱신하는 횟수
+
+  useEffect(() => {
+    let currentNumber = start
+    const counter = setInterval(() => {
+      const progress = easeOutExpo(++currentNumber / totalFrame)
+      setCount(Math.round(end * progress))
+
+      if (progress === 1) {
+        clearInterval(counter)
+      }
+    }, frameRate)
+  }, [end, frameRate, start, totalFrame])
+
+  return count
+}
